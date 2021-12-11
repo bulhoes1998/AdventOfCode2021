193af637cfa0ccdd7606fe61cd4a3b8c37f32622
bulhoes1998 <lucasbulhoes1998@outlook.com>
2021-12-11 17:45:34 -0300

    day1

--- day1/day1.js
+++ day1/day1.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 
-
 const data = fs.readFileSync('./input.txt', 'utf-8');
-const listData = data.split(/\n/);
+let listData = data.split(/\n/);
 
-let x = 0;
-let i = 0;
+const toNumbers = arr => arr.map(Number);
+listData = toNumbers(listData);
 
-while( i < listData.length) {
-    listData[i-1] < listData[i] ? x += 1 : x = x;
-    i += 1;
+const somaArray = function (arr, winLen) {
+    for (var pos = 0; pos < arr.length - winLen + 1; pos++) {
+        for (var j = 1; j < winLen; j++) {
+            arr[pos] = arr[pos] + arr[pos + j]
+        }
+    }
+    for(var i = 1; i < winLen;i++) arr.pop();
+
+    return arr;
 }
 
-console.log(x)
+const checkIncrease = function (arr) {
+    let x = 0;
+    let i = 1;
+    while (i < arr.length) {
+        arr[i - 1] < arr[i] ? x += 1 : x = x;
+        i += 1;
+    }
+    return x;
+}
+
+console.log(checkIncrease(somaArray(listData, 1)));
